429611d972d7e0bd00a3dc2071396c7ae1f57e27
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-10-27 16:20:27 +0700

    Membuat method input

--- ACT3/Program.cs
+++ ACT3/Program.cs
@@ -14,5 +14,20 @@ namespace ACT3
         int n;
         //get the number of element to strore in the array
         int i;
+
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("enter the number of element in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("/nArray should have minimum 1 and maximum 20 element. \n");
+            }
+        }
     }
 }
